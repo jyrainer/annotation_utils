63dca993ea941f299668cbc311e6b2bcb635e571
박재용 <jy.park@pia.space>
2024-11-30 18:26:38 +0900

    feat: move_video_files 함수구현 - 대분류/소분류 카테고리에 맞도록 경로에 move

--- anns_utils/site/aihub/parse_abnormal_annotations.py
+++ anns_utils/site/aihub/parse_abnormal_annotations.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import shutil
+import os.path as osp
 from glob import glob
 
 MAJOR_CATEGORY = {
@@ -8,17 +9,30 @@ MAJOR_CATEGORY = {
     "05.실신(swoon)_clip" : "Swoon"
 }
 
+
 MINOR_CATEGORY = {
     "Violence": {
-        "Violence" : ['piercing', 'pulling', 'kicking', 'threaten', 'throwing', 'punching', 'pushing'],
-        "Falldown" : ['falldown']
+        "piercing": "Violence",
+        "pulling": "Violence",
+        "kicking": "Violence",
+        "threaten": "Violence",
+        "throwing": "Violence",
+        "punching": "Violence",
+        "pushing": "Violence",
+        "falldown": "Falldown"
     },
     "Fight": {
-        "Violence" : ['piercing', 'pulling', 'kicking', 'threaten', 'throwing', 'punching', 'pushing'],
-        "Falldown" : ['falldown']
+        "piercing": "Violence",
+        "pulling": "Violence",
+        "kicking": "Violence",
+        "threaten": "Violence",
+        "throwing": "Violence",
+        "punching": "Violence",
+        "pushing": "Violence",
+        "falldown": "Falldown"
     },
-    "Falldown": {
-        "Falldown" : ['falldown']
+    "Swoon": {
+        "falldown": "Falldown"
     }
 }
 
@@ -43,21 +57,38 @@ def extract_category(category: str, xml_file_list: list[str]):
 
     print(f"Category: {category.split('/')[-1]}, Action Set: {action_set}")
 
-def move_video_files():
-    pass
-
+def move_video_files(category_dir, dst_base_dir):
+    """조건에 맞도록 파일을 옮긴다."""
+    for category in category_dir:
+        major_category = MAJOR_CATEGORY[category.split('/')[-1]]
+        file_list = get_file_list(category, "mp4")
+        
+        for file in file_list:
+            minor_category = file.split('_')[-2]
+            src = file
+            file_name = file.split('/')[-1]
+            try:
+                dst = osp.join(dst_base_dir, MINOR_CATEGORY[major_category][minor_category],file_name)
+                shutil.copy(src, dst)
+            except KeyError:
+                print(f"KeyError: {major_category, minor_category}가 존재하지 않음")
+                continue
 if __name__ == '__main__':
     ## 1. extract category
-    base_dir = "/mnt/nas_192/videos/이상행동 CCTV 영상"
+    # base_dir = "/mnt/nas_192/videos/이상행동 CCTV 영상"
 
-    category_dir = get_dir_list(base_dir)
+    # category_dir = get_dir_list(base_dir)
     
-    for category in category_dir:
-        xml_file_list = get_file_list(category, "xml")
-        extract_category(category, xml_file_list)
+    # for category in category_dir:
+    #     xml_file_list = get_file_list(category, "xml")
+    #     extract_category(category, xml_file_list)
 
     ###########################
     
     ### 2. move video files
+    base_dir = "/mnt/nas_192/videos/이상행동 CCTV 영상"
+    dst_base_dir = "/mnt/nas_192/labeling_data/completed/AIHUB_이상행동CCTV_violence_falldown"
+    raw_category_dir = get_dir_list(base_dir)
+    category_dir = [i for i in raw_category_dir if i.split('/')[-1] in MAJOR_CATEGORY.keys()]
 
-    move_video_files()
+    move_video_files(category_dir, dst_base_dir)
