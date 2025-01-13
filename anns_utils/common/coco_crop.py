a5938b842d36912e16a47672d934e0b38a9d4f45
박재용 <jy.park@pia.space>
2025-01-13 11:08:09 +0900

    feat: cropping annotation from coco annotations format 함수 수정

--- anns_utils/common/coco_crop.py
+++ anns_utils/common/coco_crop.py
@@ -1,49 +1,45 @@
-import os
 import json
-import copy
+import os
+import os.path as osp
+
 import cv2
 from tqdm import tqdm
 
 
-def crop(image_path,coco_json_path, output_path) :
-    """coco annotations 형태의 json과 이미지 source를 받아 객체를 Crop 합니다."""
-    i = 0
-    with open(coco_json_path, 'r') as file:
+def crop(image_dir, coco_json_path, output_path):
+    with open(coco_json_path) as file:
         json_data = json.load(file)
-        coco_json = copy.deepcopy(json_data)
-        
-        cate_dict = dict()
-        for cate in coco_json['categories']:
-            cate_dict[cate['id']] = cate['name']
-            os.makedirs(output_path + "/" + cate['name'], exist_ok= True)
-            
-        # 각 bbox마다 반복
-        for anns in tqdm(coco_json["annotations"]) :
-            i += 1
-            cate_name = cate_dict[anns["category_id"]]
-            image_id = anns["image_id"]                 # 이미지 고유 아이디
-            x,y,width,height = map(int,anns["bbox"])    # 박스좌표
-            for imgs in coco_json["images"] :
-                if image_id == imgs["id"] :
-                    images_path = image_path + '/' + imgs["file_name"]
-                    image = cv2.imread(images_path)
-                    
-                    if image is not None :
-                        cropped_image = image[y:y+height, x:x+width]
-                        cv2.imwrite(f"{output_path}/{cate_name}/{i}.jpg",cropped_image)
-                        #cv2.imwrite(output_path + 'nzia_' +str(i) + ".jpg",cropped_image)
-                    else :
-                        print("이미지가 없습니다")
-                    
-        
-def count_anns_imgs(coco_json_path) :
-    with open(coco_json_path, 'r') as file:
-        json_data = json.load(file)
-        coco_json = copy.copy(json_data)
-        print("anns 수:",len(coco_json["annotations"]),"imgs 수:",len(coco_json["images"]))
-        
-if __name__ == "__main__" :
-    image_path = "/home/ljj/Downloads/nzia_2/raw_image"
-    json_path = "/home/ljj/Repo/waffle_hub/scripts/coco_2.json"
-    output_path = "/home/ljj/Repo/waffle_hub/scripts/cropped"
-    crop(image_path, json_path, output_path)
+
+    cate_dict = dict()
+    for cate in json_data["categories"]:
+        cate_dict[cate["id"]] = cate["name"]
+        os.makedirs(output_path + "/" + cate["name"], exist_ok=True)
+
+    # 이미지 순회
+    for image in tqdm(json_data["images"]):
+        image_path = osp.join(image_dir, image["file_name"])
+        image_name = osp.splitext(image["file_name"])[0]
+        # error check
+        if not osp.exists(image_path):
+            print(f"Image not found: {image_path}")
+            continue
+
+        image_instance_num = 0
+        np_image = cv2.imread(image_path)
+        # annotation 순회
+        for ann in json_data["annotations"]:
+            if ann["image_id"] == image["id"]:
+                image_instance_num += 1
+                cate_name = cate_dict[ann["category_id"]]
+                x, y, width, height = map(int, ann["bbox"])
+
+                cropped_image = np_image[y : y + height, x : x + width]
+                cv2.imwrite(
+                    f"{output_path}/{cate_name}/{image_name}_{image_instance_num}.jpg", cropped_image
+                )
+
+
+def count_anns_imgs(coco_json_path):
+    with open(coco_json_path) as file:
+        coco_json = json.load(file)
+        print("anns 수:", len(coco_json["annotations"]), "imgs 수:", len(coco_json["images"]))
